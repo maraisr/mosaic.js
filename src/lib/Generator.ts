1ce9d90d2076aaa35aa72d725b36d6c18f2f8a9f
Marais Rossouw <me@maraisr.com>
2016-01-23 21:51:41 +1000

    Delaunay triangulation

--- src/lib/Generator.ts
+++ src/lib/Generator.ts
@@ -4,90 +4,6 @@ namespace Generator {
 
 	export class Delaunay {
 
-		triangulate(vertices:Array<Array<number>>):Array<Array<number>> {
-			var n:number = vertices.length,
-				indices:Array<number>,
-				st:Array<Array<number>>,
-				open:Array<any>,
-				closed:Array<any>,
-				edges:Array<number>,
-				i:number,
-				j:number,
-				a:number,
-				b:number,
-				c:number,
-				dx:number,
-				dy:number;
-
-			if (n < 3) {
-				return [];
-			}
-
-			vertices = vertices.slice(0);
-
-			indices = new Array(n);
-
-			for (i = n; i--;) {
-				indices[i] = i;
-			}
-
-			indices.sort(function (i, j) {
-				return vertices[j][0] - vertices[i][0];
-			});
-
-			st = this.sTriangle(vertices);
-			vertices.push(st[0], st[1], st[2]);
-
-			open = [this.cCircle(vertices, n + 0, n + 1, n + 2)];
-
-			for (i = indices.length; i--; edges.length = 0) {
-				c = indices[i];
-
-				for (j = open.length; j--;) {
-					dx = vertices[c][0] - open[j].x;
-
-					if (dx > 0.0 && dx * dx > open[j].r) {
-						closed.push(open[j]);
-						open.splice(j, 1);
-						continue;
-					}
-
-					if (dx * dx + dy * dy - open[j].r > EPSILON) {
-						continue;
-					}
-
-					edges.push(
-						open[j].i, open[j].j,
-						open[j].j, open[j].k,
-						open[j].k, open[j].i
-					);
-					open.splice(j, 1);
-				}
-
-				this.deDup(edges);
-
-				for (j = edges.length; j;) {
-					b = edges[--j];
-					a = edges[--j];
-					open.push(this.cCircle(vertices, a, b, c));
-				}
-			}
-
-			for (i = open.length; i--;) {
-				closed.push(open[i]);
-			}
-
-			open.length = 0;
-
-			for (i = closed.length; i--;) {
-				if (closed[i].i < n && closed[i].j < n && closed[i].k < n) {
-					open.push(closed[i].i, closed[i].j, closed[i].k);
-				}
-			}
-
-			return open;
-		}
-
 		private sTriangle(v:Array<Array<number>>):Array<Array<number>> {
 			var xmin:number = Number.POSITIVE_INFINITY,
 				ymin:number = Number.POSITIVE_INFINITY,
@@ -167,9 +83,9 @@ namespace Generator {
 				m1 * (xc - mx1) + my1 :
 				m2 * (xc - mx2) + my2;
 			}
-
 			dx = x2 - xc;
 			dy = y2 - yc;
+
 			return {i: i, j: j, k: k, x: xc, y: yc, r: dx * dx + dy * dy};
 		}
 
@@ -191,6 +107,85 @@ namespace Generator {
 					}
 				}
 			}
+		}
+
+		triangulate(vertices:Array<Array<number>>):Array<number> {
+			var n:number = vertices.length,
+				i:number, j:number, indices:Array<number>, st:Array<Array<number>>, open:Array<any>, closed:Array<any>, edges:Array<number>, dx:number, dy:number, a:number, b:number, c:number;
+
+			if (n < 3) {
+				return [];
+			}
+
+			vertices = vertices.slice(0);
+
+			indices = new Array(n);
+
+			for (i = n; i--;) {
+				indices[i] = i;
+			}
+
+			indices.sort(function (i, j) {
+				return vertices[j][0] - vertices[i][0];
+			});
+
+			st = this.sTriangle(vertices);
+			vertices.push(st[0], st[1], st[2]);
+
+			open = [this.cCircle(vertices, n + 0, n + 1, n + 2)];
+			closed = [];
+			edges = [];
+
+			for (i = indices.length; i--; edges.length = 0) {
+				c = indices[i];
+
+
+				for (j = open.length; j--;) {
+
+
+					dx = vertices[c][0] - open[j].x;
+					if (dx > 0.0 && dx * dx > open[j].r) {
+						closed.push(open[j]);
+						open.splice(j, 1);
+						continue;
+					}
+
+					dy = vertices[c][1] - open[j].y;
+
+					if (dx * dx + dy * dy - open[j].r > EPSILON) {
+						continue;
+					}
+
+					edges.push(
+						open[j].i, open[j].j,
+						open[j].j, open[j].k,
+						open[j].k, open[j].i
+					);
+					open.splice(j, 1);
+				}
+
+				this.deDup(edges);
+
+				for (j = edges.length; j;) {
+					b = edges[--j];
+					a = edges[--j];
+					open.push(this.cCircle(vertices, a, b, c));
+				}
+			}
+
+			for (i = open.length; i--;) {
+				closed.push(open[i]);
+			}
+
+			open.length = 0;
+
+			for (i = closed.length; i--;) {
+				if (closed[i].i < n && closed[i].j < n && closed[i].k < n) {
+					open.push(closed[i].i, closed[i].j, closed[i].k);
+				}
+			}
+
+			return open;
 		}
 	}
 }
